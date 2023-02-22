7fced8fadb6c7abfc03ceb1a1816717bf4769f60
NthTryMan <111993380+qwetry4444@users.noreply.github.com>
2023-02-22 12:31:21 +0300

    Update switchTheme.js

--- main_page/switchTheme.js
+++ main_page/switchTheme.js
@@ -1,5 +1,5 @@
-let CurrentPathToCSS = "css/styleLightTheme/main_light.css";
-let AnotherPathToCSS = "css/styleDarkTheme/main_Dark.css";
+let CurrentPathToCSS = "css/main_light.css";
+let AnotherPathToCSS = "css/main_Dark.css";
 
 function switch_theme(){
     let temp = CurrentPathToCSS;
